fa8a74b9f5c7d1838fcf905fe56f4e1d7b8ed0c4
Tim Sze <timmysze@gmail.com>
2013-04-28 12:17:44 -0700

    fixed buffer, added playing

--- public/js/recorder.js
+++ public/js/recorder.js
@@ -1,159 +1,240 @@
-// MAYBE: stick this in some event handler for a button click
 var socket = io.connect('http://127.0.0.1')
   , tapeStart
   , tapeEnd
   , totSecs
-  , currDrawSec = 0
+  , currBar = 0
   , currDrawTime
   , drawRateSecs
-  , drawQueue = [];
+  , playSpeedSecs = 2
+  , tweetQueue = []
+  , denFn
+  , cumDenFn;
+
+// Graph variables
+var numBars = 100;
+var w = Math.floor(document.querySelector('.center').scrollWidth * 0.9);
+var splitHeight = 180;
+var underBarsHAdj = 0.75;
+var h = splitHeight * (1 + underBarsHAdj);
+var scale = 1;
+var unitPadding = 2;
+var unitWidth = Math.floor(w / numBars - unitPadding);
+var unitHeight = splitHeight / 100 * scale;
+
+//Create SVG element
+var svg = d3.select('.center')
+            .insert('svg')
+            .attr('width', w)
+            .attr('height', h);
 
 var drawHeader = function (query, tapeStart, screen_name) {
   var queryNode = document.querySelector('.query')
     , tapeStartNode = document.querySelector('.tapeStart')
     , nameNode = document.querySelector('.name')
-    , dateString = moment(tapeStart).format('M.D.YYYY')
-    , startTimeString = moment(tapeStart).format('HH.mm.ss');
+    , dateString = moment(tapeStart).format('M/D/YYYY')
+    , startTimeString = moment(tapeStart).format('HH:mma');
 
   queryNode.textContent = query;
   tapeStartNode.textContent = dateString + ' ' + startTimeString;
-  nameNode.textContent = '@' + screen_name;
+
+  // nameNode.textContent = '@' + screen_name;
 };
 
-socket.emit('record', { q: 'boston', stamp: new Date() });
-
-socket.on('segment', function (data) {
-  data.segment.forEach(function (tweet) {
-    console.log('tweet to draw: ' + tweet);
-    drawQueue.push(tweet);
-  });
-  if (!startedDrawing) { startDrawing(); }
-});
+// MAYBE: stick this in some event handler for a button click
+// TODO: do something about this data...
+socket.emit('record', { q: 'FML', stamp: new Date() });
 
 socket.on('header', function (data) {
   tapeStart = data.tapeStart;
   tapeEnd = data.tapeEnd;
-  totSecs = data.tapeTotSecs;
   drawHeader(data.query, data.tapeStart, data.screen_name);
 });
 
+socket.on('buffer', function (data) {
+  var scaleNode = document.querySelector('.scale');
 
+  data.buffer.forEach(function (tweet) {
+    tweetQueue.push(tweet);
+  });
+  denFn = data.denFn;
+  cumDenFn = data.cumDenFn;
+  drawRateSecs = data.secsPerBar;
+  scale = data.scale;
+  scaleNode.textContent = ' scale: ' + scale + 'x';
+  unitHeight = splitHeight / 100 * scale;
+  startDrawing();
+});
 
+socket.on('segment', function (data) {
+  data.segment.forEach(function (tweet) {
+    tweetQueue.push(tweet);
+  });
+  denFn = data.denFn;
+  cumDenFn = data.cumDenFn;
+});
+
+socket.on('finished', function () {
+
+});
 
 // TODO: add a textbox for entry of search query
   // in place editing of search term?
 
-
-
-// Graph variables
-var w = 860;
-var splitHeight = 180;
-var underBarsHeightAdjust = 0.75;
-var h = splitHeight * (1 + underBarsHeightAdjust);
-var unitPadding = 2;
-var unitWidth = w / numBars - unitPadding;
-var unitHeight = splitHeight / 100;
-var numBars = 100;
-var mod = Math.ceil(totSecs / numBars);
-
-drawRateSecs = mod;
-
-var drawnTweets = [];
-
-
-//Create SVG element
-var svg = d3.select("body")
-            .append("svg")
-            .attr("width", w)
-            .attr("height", h);
-
 // MAYBE: refactor so that this takes those params for drawing
 // MAYBE: move this into a module
-function drawGraph(data) {
-  var barsTracker = {};
-  // console.log('mod: ', mod);
+var drawBar = function (data) {
 
-  var barNumber = function (time) {
-    // console.log('tapestart: ' + moment(tapeStart).format('hh.mm.ss'));
-    // console.log('tape this one: ' + moment(time).format('hh.mm.ss'));
-    var a = Math.floor(moment(time).diff(moment(tapeStart), 'seconds') / mod);
-    // console.log('bar num: ',a);
-    return a;
-  };
+  // TODO: refactor so that currBar and stuff are params
+  var overHeight = Math.floor(data[0] * unitHeight)
+    , underHeight = Math.floor(data[0] * unitHeight * underBarsHAdj)
+    , overY = splitHeight - overHeight
+    , underY = splitHeight
+    , currX = currBar * (unitWidth + unitPadding);
 
-  var barHeight = function (time) {
-    var barNum = barNumber(time);
-    if (!barsTracker[barNum]) { barsTracker[barNum] = 0; }
-    return barsTracker[barNum]++;
-  };
-
-  // MAYBE: refactor so don't need to redraw...
-  // Clear bars
-  svg.selectAll("rect").remove();
-
-  // Draw over bars
-  svg.selectAll("rect.over")
+  svg.selectAll('rect.over' + currBar)
       .data(data)
       .enter()
-      .append("rect")
-      .attr("x", function(d, i) {
-        return barNumber(d.created_at) * (unitWidth + unitPadding);
-      })
-      .attr("y", function(d){
-        return splitHeight - barHeight(d.created_at) * unitHeight;
-      })
-      .attr("fill", "#00C3FF")
-     // .attr("height", 0)
-     // .transition()
-     //   .duration(800)
-     //   .ease('')
-      .attr("width", unitWidth)
-      .attr("height", unitHeight);
+      .append('rect')
+      .attr('class', 'over' + currBar)
+      .attr('fill', '#000000')
+      // .attr('fill', '#00C3FF')
+      .attr('x', currX)
+      .attr('y', splitHeight)
+      .attr('height', 0)
+      .transition()
+      .duration(800)
+      .ease('')
+      .attr('y', overY)
+      .attr('width', unitWidth)
+      .attr('height', overHeight);
 
-  // Draw under bars
-  svg.selectAll("rect.under")
+  svg.selectAll('rect.under' + currBar)
       .data(data)
       .enter()
-      .append("rect")
-      .attr("x", function(d, i) {
-        return barNumber(d.created_at) * (unitWidth + unitPadding);
-      })
-      .attr("y", function(d){
-        return splitHeight + (barHeight(d.created_at) - 1) * unitWidth;
-      })
-      .attr("width", unitWidth)
-      .attr("height", unitHeight)
-      .attr("fill", "#00C3FF")
-      .attr("opacity", "0.2");
-}
-
-var startedDrawing = false;
-var startDrawing = function () {
-  setInterval(function () {
-    if (currDrawSec++ > totSecs) { clearInterval(); }
-    currDrawTime = moment(tapeStart).add('seconds', currDrawSec);
-    // console.log('tape start: ' + moment(tapeStart).format('hh.mm.ss'));
-    // console.log('current drawtime: ' + moment(currDrawTime).format('hh.mm.ss'));
-    while (drawQueue[0] && (moment(drawQueue[0].created_at) <= currDrawTime)) {
-      var aaa = drawQueue.shift();
-      // console.log(aaa);
-      drawnTweets.push(aaa);
-    }
-    drawGraph(drawnTweets);
-  }, drawRateSecs * 1000);
-  startedDrawing = true;
+      .append('rect')
+      .attr('class', 'under' + currBar)
+      .attr('fill', '#000000')
+      // .attr('fill', '#00C3FF')
+      .attr('opacity', '0.2')
+      .attr('x', currX)
+      .attr('y', underY)
+      .attr('width', unitWidth)
+      .attr('height', 0)
+      .transition()
+      .duration(800)
+      .ease('')
+      .attr('height', underHeight);
 };
 
-  // setInterval(function () {
-  //   if (currDrawSec++ > totSecs) { clearInterval(); }
-  //   currDrawTime = moment(tapeStart).add('seconds', currDrawSec);
-  //   // console.log('tape start: ' + moment(tapeStart).format('hh.mm.ss'));
-  //   // console.log('current drawtime: ' + moment(currDrawTime).format('hh.mm.ss'));
-  //   while (drawQueue[0] && (moment(drawQueue[0].created_at) <= currDrawTime)) {
-  //     var aaa = drawQueue.shift();
+var drawPlayed = function (playNum) {
 
-  //   }
-  //   drawGraph();
-  // }, drawRateSecs * 1000);
+  // init the progress bar if not already
+  svg.selectAll('rect.overProg').remove();
+  svg.selectAll('rect.underProg').remove();
+  var progOverBar = svg.selectAll('rect.overProg')
+                    .data([0])
+                    .enter()
+                    .append('rect')
+                    .attr('class', 'overProg');
+  var progUnderBar = svg.selectAll('rect.underProg')
+                    .data([0])
+                    .enter()
+                    .append('rect')
+                    .attr('class', 'underProg');
 
 
+  // reset all the bars colors
+  svg.selectAll('rect')
+      .attr('fill', '#000000');
+
+  // reset the progBar
+  progOverBar.attr('height', 0);
+  progUnderBar.attr('height', 0);
+
+  // playNum less than zero means show none
+  if (playNum < 0) {
+    // just reset
+    return;
+  }
+
+  // use the cdf to calc which bars played
+  // and fraction of which on i'm playing...
+  // cdf is cumDenFn
+  var upToBar = 0;
+  for (var i = 0; i < numBars; i++) {
+    if (playNum <= cumDenFn[i]) {
+      upToBar = i;
+      i = numBars;
+    }
+  }
+
+  for (var i = 0; i < upToBar; i++) {
+    svg.selectAll('rect.over'+i).attr('fill', '#00C3FF');
+    svg.selectAll('rect.under'+i).attr('fill', '#00C3FF');
+  }
+
+  var blueUnits;
+
+  // TODO: refactor to turnery operator
+  if (upToBar) {
+    blueUnits = playNum - cumDenFn[upToBar - 1];
+  } else {
+    blueUnits = playNum;
+  }
+
+  var overHeight = Math.floor(blueUnits * unitHeight);
+  var underHeight = Math.floor(blueUnits * unitHeight * underBarsHAdj);
+  var overY = splitHeight - overHeight;
+  var underY = splitHeight;
+  var currX = upToBar * (unitWidth + unitPadding);
+
+  progOverBar.data([blueUnits])
+              .attr('fill', '#00C3FF')
+              .attr('x', currX)
+              .attr('y', overY)
+              .attr('width', unitWidth)
+              .attr('height', overHeight);
+
+  progUnderBar.data([blueUnits])
+              .attr('fill', '#BAEFFF')
+              // .attr('opacity', '0.2')
+              .attr('x', currX)
+              .attr('y', underY)
+              .attr('width', unitWidth)
+              .attr('height', underHeight);
+};
+
+var startPlaying = function () {
+  var counter = 0;
+  setInterval(function () {
+    // TODO: make it such that can't go past cdf of currbar!
+    // GENIUS FOR df and cdf!!
+    if (tweetQueue[counter]) {
+      var tweetsNode = document.querySelector('.tweets')
+        , nameNode = document.querySelector('.name')
+        , newTweetNode = document.createElement('div');
+      // console.log(tweetsNode);
+      newTweetNode.textContent = tweetQueue[counter].text;
+      tweetsNode.appendChild(newTweetNode);
+      newTweetNode.scrollIntoView();
+      // tweetsNode.textContent = tweetQueue[counter].text;
+      nameNode.textContent = '@'+ tweetQueue[counter].screenName;
+      // console.log(moment(tweetQueue[counter].created_at).format());
+      // console.log(tweetQueue[counter].twitterID);
+      drawPlayed(counter);
+      counter++;
+    }
+  }, playSpeedSecs * 1000);
+};
+
+var startDrawing = function () {
+  if(denFn[currBar]){
+    drawBar([denFn[currBar]]);
+    currBar++;
+  }
+  startPlaying();
+  setInterval(function () {
+    drawBar([denFn[currBar]]);
+    if (currBar++ > numBars) {   clearInterval(); }
+  }, drawRateSecs * 1000);
+};
--- record.js
+++ record.js
@@ -1,16 +1,31 @@
 var auth = require('./auth')
   , oa = auth.makeOAuth()
   , moment = require('moment')
-  , tape = []
-  , tapeIndex = 0
+  // , tapeIndex = 0
   , tapeTimelinePos = null
   , tapeStart = null
-  , tapeBufferSecs = 30 // twitter search delay is about 7 seconds
-  , tapeElapsedSecs = null
+  // , tapeBufferSecs = 30 // twitter search delay is about 7 seconds
   , tapeTotMins = 10
-  , tapeTotSecs = tapeTotMins * 60
-  , recordRateSecs = 10
-  , tapeDeck;
+  , recordRateSecs = 8
+  , tapeDeck
+  , numBars = 100
+  , secsPerBar = Math.ceil(tapeTotMins * 60 / numBars)
+  , scale = 1
+  , tape = []
+  , denFn = []
+  , cumDenFn = [];
+
+for (var i = 0; i < 100; i++) {
+  denFn.push(0);
+  cumDenFn.push(0);
+}
+
+var calcCumDenFn = function () {
+  cumDenFn[0] = denFn[0];
+  for (var i = 1, l = denFn.length; i < l; i++) {
+    cumDenFn[i] = cumDenFn[i-1] + denFn[i];
+  }
+};
 
 exports.index = function (req, res) {
   res.render('recorder');
@@ -19,23 +34,59 @@ exports.index = function (req, res) {
 exports.recordTrack = function (sock, sess, q, stamp) {
   if (!sess.hasOwnProperty('oAuthVars')) { return; }
 
-  tapeStart = moment(stamp).subtract('seconds', tapeBufferSecs).utc().format();
-  tapeEnd = moment(tapeStart).add('minutes', tapeTotMins).utc().format();
+  tapeStart = moment(stamp).subtract('m', 1).startOf('minute').utc().format();
+  tapeEnd = moment(tapeStart).add('m', tapeTotMins).utc().format();
+
+  var calcBar = function (time) {
+    console.log('cal bar');
+    console.log(Math.floor(moment(time).diff(moment(tapeStart), 'seconds') / secsPerBar));
+    return Math.floor(moment(time).diff(moment(tapeStart), 'seconds') / secsPerBar);
+  };
+
+  var calcScale = function (array) {
+    var safety = 0.75
+      , maxUnits = 100;
+
+    var maxVal = array.reduce(function (prev, curr) {
+      return Math.max(prev, curr);
+    }, 1);
+    console.log('max: ' + maxVal);
+    return Math.floor(maxUnits / maxVal * safety);
+  };
+
+  var getTweets = function (query, callback) {
+    oa.getProtectedResource(query, 'GET'
+        , sess.oAuthVars.oauth_access_token
+        , sess.oAuthVars.oauth_access_token_secret
+        , callback);
+  };
+
+  var formatTweet = function (element) {
+    var tweet = {};
+    // tweet.tapeID = tapeIndex;
+    tweet.created_at = moment(element.created_at).utc().format();
+    console.log(tweet.created_at);
+    tweet.twitterID = element.id_str;
+    tweet.text = element.text;
+    tweet.userID = element.user.id_str;
+    tweet.userName = element.user.name;
+    tweet.screenName = element.user.screen_name;
+    tweet.location = element.user.location;
+    tweet.entities = element.entities;
+    return tweet;
+  };
 
   var recordSegment = function () {
+    console.log('tape time pos: ' + tapeTimelinePos);
     var query = 'https://api.twitter.com/1.1/search/tweets.json?' +
-                'q=' + q + '&count=100' + '&result_type=recent';
+                'q=' + q + '&count=100' + '&result_type=recent' +
+                '&since_id=' + tapeTimelinePos;
 
-    if (tapeTimelinePos) { query += '&since_id=' + tapeTimelinePos; }
-
-    console.log(query);
-
-    oa.getProtectedResource(query, 'GET'
-    , sess.oAuthVars.oauth_access_token
-    , sess.oAuthVars.oauth_access_token_secret
-    , function (error, data, response) {
+    var tweethandler = function (error, data, response) {
+      console.log('handling stuff');
       var tweetStream
         , tweet
+        , barNum
         , streamLength
         , segment = [];
 
@@ -46,46 +97,90 @@ exports.recordTrack = function (sock, sess, q, stamp) {
 
         tweetStream.forEach(function (e, index) {
           element = tweetStream[streamLength - 1 - index];
-          if (moment(element.created_at) >= moment(tapeStart)) {
-            tweet = {};
-            tweet.tapeID = tapeIndex;
-            tweet.created_at = moment(element.created_at).utc().format();
-            console.log(tweet.created_at);
-            tweet.twitterID = element.id_str;
-            tweet.text = element.text;
-            tweet.userID = element.user.id_str;
-            tweet.userName = element.user.name;
-            tweet.screenName = element.user.screen_name;
-            tweet.location = element.user.location;
-            tweet.entities = element.entities;
-
-            tapeIndex++;
+          // if (moment(element.created_at) >= moment(tapeStart)) {}
+          if (moment(element.created_at) > moment(tapeEnd)) {
+            clearInterval();
+            // TODO: emit finished to client
+            sock.emit('finished');
+            // TODO: send tape to mongo, with headers
+            // TODO: store the heights of the bars for quick drawing
+          } else {
+            tweet = formatTweet(element);
+            // tapeIndex++;
             tape.push(tweet);
             segment.push(tweet);
+            barNum = calcBar(tweet.created_at);
+            denFn[barNum]++;
           }
         });
 
-        // TODO: maybe fix this...
-        tapeElapsedSecs += recordRateSecs;
-
-        console.log(tapeElapsedSecs);
-        console.log(tape.length);
-
-        sock.emit('segment', { segment: segment });
-
-        if (tape.length > 0){
-          tapeTimelinePos = tape[tapeIndex-1].twitterID;
-
-          // Finished rolling
-          if (tapeElapsedSecs / 60 >= tapeTotMins) {
-            clearInterval(tapeDeck);
-            // TODO: emit finished to client
-            // TODO: send tape to mongo, with headers
-            // TODO: store the heights of the bars for quick drawing
-          }
-        }
+        calcCumDenFn();
+        sock.emit('segment', {
+          segment: segment,
+          denFn: denFn,
+          cumDenFn: cumDenFn
+        });
+        tapeTimelinePos = tape[tape.length-1].twitterID;
       }
-    });
+    };
+
+    getTweets(query, tweethandler);
+  };
+
+  var recordBuffer = function () {
+    var buffer = []
+      , bufferFull = false;
+
+    // Grab historical tweets until fill buffer
+    (function iterator (max_id) {
+      var tweetStream
+        , tweet
+        , barNum
+        , query = 'https://api.twitter.com/1.1/search/tweets.json?' +
+                  'q=' + q + '&count=100' + '&result_type=recent';
+
+      if (max_id) { query += '&max_id=' + max_id; }
+
+      var tweetHandler = function (error, data, response) {
+        if (error) { console.log('error', error); }
+        else {
+          tweetStream = JSON.parse(data).statuses;
+          // MAYBE: if max_id, then skip the first one?
+
+          tweetStream.forEach(function (element, index) {
+            if (moment(element.created_at) >= moment(tapeStart)) {
+              tweet = formatTweet(element);
+              tape.unshift(tweet);
+              buffer.unshift(tweet);
+              barNum = calcBar(tweet.created_at);
+              denFn[barNum]++;
+            } else {
+              bufferFull = true;
+            }
+          });
+        }
+
+        if (!bufferFull) {
+          console.log('yo: ' + tape[0].twitterID);
+          iterator(tape[0].twitterID);
+        } else {
+          // MAYBE: handling full buffer w/o any data
+          calcCumDenFn();
+          tapeTimelinePos = tape[tape.length - 1].twitterID;
+          scale = calcScale(denFn);
+          sock.emit('buffer', {
+            buffer: buffer,
+            denFn: denFn,
+            cumDenFn: cumDenFn,
+            secsPerBar: secsPerBar,
+            scale: scale
+          });
+          tapeDeck = setInterval(recordSegment, recordRateSecs * 1000);
+        }
+      };
+
+      getTweets(query, tweetHandler);
+    })();
   };
 
   var sendHeader = function () {
@@ -95,7 +190,6 @@ exports.recordTrack = function (sock, sess, q, stamp) {
     header.query = q;
     header.tapeStart = tapeStart;
     header.tapeEnd = tapeEnd;
-    header.tapeTotSecs = tapeTotSecs;
     oa.getProtectedResource(url, 'GET'
         , sess.oAuthVars.oauth_access_token
         , sess.oAuthVars.oauth_access_token_secret
@@ -107,6 +201,5 @@ exports.recordTrack = function (sock, sess, q, stamp) {
   };
 
   sendHeader();
-  recordSegment();
-  tapeDeck = setInterval(recordSegment, recordRateSecs * 1000);
+  recordBuffer();
 };
